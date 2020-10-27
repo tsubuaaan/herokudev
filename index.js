5953b9bc50126b865f5044ad6df6e6119db12b96
tsubuaaan <tsubuan@hotmail.com>
2020-10-27 08:11:39 +0000

    index.jsの文字列に2020を追加した

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let port = process.env.PORT || 5000;
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World 2020\n');
 }).listen(port, () => console.log('Server http://localhost:'+port));
